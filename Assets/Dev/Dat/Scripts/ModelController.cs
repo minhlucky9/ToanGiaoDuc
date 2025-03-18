accb90bc6af1e7643f155dcd8f974165c615453d
TanDat <145632083+DatGameDev26@users.noreply.github.com>
2025-03-18 11:11:10 +0700

    Cập nhật Dạng bài Hình học

--- Assets/Dev/Dat/Scripts/ModelController.cs
+++ Assets/Dev/Dat/Scripts/ModelController.cs
@@ -1,20 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace DatDev
+namespace BlockNumber
 {
     public class ModelController : MonoBehaviour
     {
-        void Start()
-        {
+        [SerializeField] float rotationSpeed = 5f;
+        [SerializeField] RectTransform imageRectTransform;
+        [SerializeField] Transform modelContainerT;
 
-        }
+        Vector3 lastInputPosition;
 
         void Update()
         {
+            if (!IsMouseOverUI() || modelContainerT == null) return;
 
+            if (Input.GetMouseButton(0) || Input.touchCount > 0)
+            {
+                Vector3 deltaInput = GetCurrentInputPosition() - lastInputPosition;
+                float rotationX = deltaInput.x * rotationSpeed * Time.deltaTime;
+                float rotationY = deltaInput.y * rotationSpeed * Time.deltaTime;
+                modelContainerT.Rotate(Vector3.up, -rotationX, Space.World);
+                modelContainerT.Rotate(Vector3.right, rotationY, Space.World);
+                lastInputPosition = GetCurrentInputPosition();
+            }
+            else lastInputPosition = GetCurrentInputPosition();
+        }
+
+        Vector3 GetCurrentInputPosition()
+        {
+            if (Input.touchCount > 0) return Input.GetTouch(0).position;
+            else return Input.mousePosition;
+        }
+
+        bool IsMouseOverUI()
+        {
+            return RectTransformUtility.RectangleContainsScreenPoint(
+                imageRectTransform, Input.mousePosition, Camera.main);
         }
     }
-
 }
