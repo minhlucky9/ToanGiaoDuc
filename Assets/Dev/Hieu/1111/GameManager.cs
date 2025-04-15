e7ff3c0a62b39f836d1d9f720beb2abbcba5c793
hieu163 <204822521+hieu1632@users.noreply.github.com>
2025-04-15 11:13:12 +0700

    bai2

---
+++ Assets/Dev/Hieu/1111/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace MathConnection
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+        public GameObject scorePanel;
+        public TextMeshProUGUI correctText;
+        public TextMeshProUGUI wrongText;
+        public TextMeshProUGUI mistakeText;
+        public TextMeshProUGUI timeText;
+        public TimerScript timerScript;
+
+        public int mistakeCount = 0;
+
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(gameObject);
+
+            scorePanel.SetActive(false);
+        }
+
+        public void AddMistake()
+        {
+            mistakeCount++;
+            Debug.Log($"[GameManager] Số lỗi hiện tại: {mistakeCount}");
+        }
+
+        public void ShowScore()
+        {
+            int correctCount = 0;
+            int wrongCount = 0;
+
+            var draggables = FindObjectsOfType<DraggableObject>();
+
+            foreach (var draggable in draggables)
+            {
+                NumberSlot slot = draggable.currentConnectedSlot;
+                bool isCorrectNow = (slot != null && draggable.objectCount == slot.numberSlot);
+
+                if (isCorrectNow)
+                    correctCount++;
+                else
+                    wrongCount++;
+            }
+
+            timerScript.StopTimer();
+
+            correctText.text = $" {correctCount}";
+            wrongText.text = $" {wrongCount}";
+            mistakeText.text = $" {mistakeCount}";
+            timeText.text = $" {timerScript.GetFormattedTime()}";
+
+            Debug.Log($"[GameManager] Kết quả: Đúng: {correctCount} - Sai: {wrongCount} - Lỗi: {mistakeCount}");
+
+            scorePanel.SetActive(true);
+            ClearAllLines();
+        }
+
+        public void ClearAllLines()
+        {
+            var draggables = FindObjectsOfType<DraggableObject>();
+            foreach (var draggable in draggables)
+            {
+                draggable.ClearLineRenderer();
+            }
+            Debug.Log("[GameManager] Đã xóa tất cả đường nối.");
+        }
+
+        public void ResetGame()
+        {
+            mistakeCount = 0;
+            var draggables = FindObjectsOfType<DraggableObject>();
+            foreach (var draggable in draggables)
+            {
+                draggable.ClearLineRenderer();
+                draggable.transform.position = draggable.startPoint.position;
+                draggable.currentConnectedSlot = null;
+            }
+
+            Debug.Log("[GameManager] Đã reset trò chơi.");
+
+            scorePanel.SetActive(false);
+        }
+
+        public void AutoCorrectConnections()
+        {
+            var draggables = FindObjectsOfType<DraggableObject>();
+            var slots = FindObjectsOfType<NumberSlot>();
+
+            Debug.Log("[GameManager] Đang tự động kết nối...");
+
+            foreach (var draggable in draggables)
+            {
+                if (draggable.currentConnectedSlot != null)
+                {
+                    draggable.currentConnectedSlot.connectedObjects.Remove(draggable);
+                    draggable.currentConnectedSlot = null;
+                }
+
+                NumberSlot correctSlot = null;
+                foreach (var slot in slots)
+                {
+                    if (draggable.objectCount == slot.numberSlot)
+                    {
+                        correctSlot = slot;
+                        break;
+                    }
+                }
+
+                if (correctSlot != null)
+                {
+                    correctSlot.AddConnection(draggable);
+                    draggable.ConnectToSlot(correctSlot);
+                    Debug.Log($"[GameManager] Đối tượng {draggable.name} được kết nối với {correctSlot.name}");
+                }
+                else
+                {
+                    Debug.LogWarning($"[GameManager] Không tìm thấy số đúng cho {draggable.name}");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Dev/Hieu/2222/RandomColorShapesUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ColorPaint
+{
+    public class RandomColorShapesUI : MonoBehaviour
+    {
+        public Image[] shapes;
+        private Color[] colors = {
+            Color.red,
+            Color.yellow,
+            Color.green,
+            Color.blue,
+            new Color(1.0f, 0.75f, 0.8f),
+            new Color(0.5f, 0.0f, 0.5f),
+            new Color(1.0f, 0.5f, 0.0f)
+        };
+
+        void Start()
+        {
+            AssignRandomColors();
+        }
+
+        void AssignRandomColors()
+        {
+            foreach (Image shape in shapes)
+            {
+                shape.color = colors[Random.Range(0, colors.Length)];
+            }
+        }
+    }
+}
