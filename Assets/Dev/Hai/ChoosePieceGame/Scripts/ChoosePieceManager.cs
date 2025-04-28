122e5f827641ace7876819df53aeea7582746522
NgHngHai <honghaiae@gmail.com>
2025-04-28 09:37:35 +0700

    finished ChoosePieceGame

---
+++ Assets/Dev/Hai/ChoosePieceGame/Scripts/ChoosePieceManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChoosePiece
+{
+    public class ChoosePieceManager : MonoBehaviour
+    {
+        GameObject[] cornerPieces;
+        CornerPiece correctPiece;
+        GameObject[] answerButtons;
+
+        private Button btnComplete;
+
+        public int correctAnswers = 0;
+        public int wrongAttemps = 0;
+        private float startTime = 0;
+        private float time = 0;
+        private bool isStarted = false;
+
+        // Chỉ chữa bài 1 lần
+        private bool hasShownAnswer = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cornerPieces = GameObject.FindGameObjectsWithTag("CornerPiece");
+            answerButtons = GameObject.FindGameObjectsWithTag("BtnAnswer");
+
+            foreach (GameObject piece in cornerPieces)
+            {
+                CornerPiece pieceScript = piece.GetComponent<CornerPiece>();
+
+                if (pieceScript.isCorrectPiece)
+                {
+                    correctPiece = pieceScript;
+                    break;
+                }
+            }
+
+            // Lấy btn hoàn thành
+            btnComplete = GameObject.FindGameObjectWithTag("BtnComplete").GetComponent<Button>();
+            btnComplete.onClick.AddListener(Submit);
+
+            StartTimer();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isStarted)
+            {
+                time = Time.time - startTime;
+            }
+        }
+
+        // Đổi số giây thành định dạng phút'giây''
+        public string ConvertTime(int time)
+        {
+            int min = 0, sec = 0;
+            string minString, secString;
+
+            min = time / 60;
+            sec = time % 60;
+
+            if (sec < 10)
+            {
+                secString = "0" + sec + "''";
+            }
+            else
+            {
+                secString = sec + "''";
+            }
+
+            if (min < 10)
+            {
+                minString = "0" + min + "'";
+            }
+            else
+            {
+                minString = min + "'";
+            }
+
+            return minString + secString;
+        }
+
+        public void Submit()
+        {
+            Debug.Log("Correct answers: " + correctAnswers + ", Wrong attempts: " + wrongAttemps + ", Time: " + ConvertTime((int)time));
+        }
+
+        public void StartTimer()
+        {
+            // Đánh dấu thời gian bắt đầu
+            startTime = Time.time;
+            isStarted = true;
+        }
+
+        // Chữa bài
+        public void CorrectWork()
+        {
+            if (!hasShownAnswer)
+            {
+                correctPiece.MoveToBase();
+                hasShownAnswer = true;
+            }
+        }
+
+        public void DisableButtons()
+        {
+            foreach (GameObject objectButton in answerButtons)
+            {
+                Button button = objectButton.GetComponent<Button>();
+                button.interactable = false;
+            }
+        }
+    }
+}
---
+++ Assets/Dev/Hai/ChoosePieceGame/Scripts/PieceInspector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChoosePiece
+{
+    public class PieceInspector : MonoBehaviour
+    {
+        public float rotationSpeed = 300f;
+        private bool isInspecting = false;
+
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                // Ưu tiên đối tượng khi click
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Inspectables")))
+                {
+
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        isInspecting = true;
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (!isInspecting) return;
+                isInspecting = false;
+            }
+
+            if (isInspecting)
+            {
+                float rotX = -Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+                float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+                Quaternion deltaRotation = Quaternion.Euler(rotY, rotX, 0);
+                transform.rotation = deltaRotation * transform.rotation;
+            }
+        }
+    }
+}
+
